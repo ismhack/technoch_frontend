5d22d69d3416426221dcb691587d8c64fd0f55bd
Ismael <ismhack@msn.com>
2025-09-25 12:06:23 -0500

    feat: improved footer to display only when toggled.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {}
+export class FooterComponent {
+  isFooterExpanded = false;
+
+  toggleFooter() {
+    this.isFooterExpanded = !this.isFooterExpanded;
+  }
+}
