d4f05273912ffd2ede823400b8220bbe8a88d69d
Ismael <ismhack@msn.com>
2025-10-20 15:48:55 -0500

    feat: update request headers

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8', 'access-control-request-method': 'GET', 'Origin': "'"+ window.location.origin + "'"})
 
- constructor(private http: HttpClient) { }
+ constructor(private http: HttpClient) {
+  }
  getMessage() {
     return this.http.get(`/`);
   }
 
   postContact(message:any){
 
-    return this.http.post(`/api/contact`, message);
+    return this.http.post(`/api/contact`, message, 
+      {headers:
+        this.headers
+      }
+    );
   }
 }
